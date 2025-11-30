197ed312a9b9e44ff005ccb86948487edebe8588
Vadim <vadik.mahnev@yandex.ru>
2025-11-30 23:32:06 +0300

    feat(migrations):add migrations for users database

---
+++ migrations/000001_create_user_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users {
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+};
+
+ALTER TABLE users ADD CONSTRAINT check_age CHECK (age > 0);
+
+CREATE INDEX IF NOT EXISTS idx_users_id ON users(id);
+CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);

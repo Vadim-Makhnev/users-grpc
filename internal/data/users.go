f9a316e1d91cc21df122e131696b8ef57d91cd9b
Vadim <vadik.mahnev@yandex.ru>
2025-12-02 00:19:12 +0300

    feat(internal):add GetAll storage method

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,18 +4,19 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/Vadim-Makhnev/grpc/internal/validator"
 )
 
 type User struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Age       int32     `json:"age"`
-	CreatedAt time.Time `json:"-"`
-	Version   int32     `json:"version"`
+	ID        int64
+	Name      string
+	Email     string
+	Age       int32
+	CreatedAt time.Time
+	Version   int32
 }
 
 type UserModel struct {
@@ -38,7 +39,7 @@ func (u *UserModel) CreateUser(user *User) error {
 
 func (u *UserModel) GetUser(id int64) (*User, error) {
 	query := `
-		SELECT id, name, email, age, version
+		SELECT id, name, email, age, created_at, version
 		FROM users
 		WHERE id = $1
 		`
@@ -52,6 +53,7 @@ func (u *UserModel) GetUser(id int64) (*User, error) {
 		&user.Name,
 		&user.Email,
 		&user.Age,
+		&user.CreatedAt,
 		&user.Version,
 	)
 
@@ -68,9 +70,60 @@ func (u *UserModel) GetUser(id int64) (*User, error) {
 
 }
 
+func (u *UserModel) GetAll(filters Filters) ([]*User, MetaData, error) {
+	query := fmt.Sprintf(`
+		SELECT count(*) OVER(), id, name, email, age, created_at, version
+		FROM users
+		ORDER BY %s %s, id ASC
+		LIMIT $1 OFFSET $2`, filters.sortColumn(), filters.sortDirection())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{filters.limit(), filters.offset()}
+
+	rows, err := u.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, MetaData{}, err
+	}
+
+	defer rows.Close()
+
+	totalRecords := 0
+
+	users := []*User{}
+
+	for rows.Next() {
+		var user User
+
+		err := rows.Scan(
+			&totalRecords,
+			&user.ID,
+			&user.Name,
+			&user.Email,
+			&user.Age,
+			&user.CreatedAt,
+			&user.Version,
+		)
+		if err != nil {
+			return nil, MetaData{}, err
+		}
+
+		users = append(users, &user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, MetaData{}, err
+	}
+
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
+	return users, metadata, err
+}
+
 func ValidateUser(v *validator.Validator, user *User) {
 	v.Check(user.Name != "", "name", "must be provided")
-
 	v.Check(user.Email != "", "email", "must be provided")
+	v.Check(user.Age > 0, "age", "must be greater than 0")
 	v.Check(validator.Matches(user.Email, validator.EmailRX), "email", "must be a valid email address")
 }
